e0e5931c311546b02afb2e8fadf4431298fefe57
James Du <james.du@mobisocial.us>
2024-06-03 12:29:36 +0800

    chore: add github action

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  basePath: "/mono-github-search",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
