dbb52e7f2257abac593cefe17176a709fd93b655
James Du <james.du@mobisocial.us>
2024-06-04 23:52:34 +0800

    feat: add starRepo api

--- packages/github-service/src/index.ts
+++ packages/github-service/src/index.ts
@@ -20,11 +20,21 @@ type SearchRepoResponse = {
     html_url: string;
     full_name: string;
     description: string;
+    star?: boolean;
   }[];
   incomplete_results: boolean;
 };
 
-export async function searchRepo(query: string, page = 1) {
+function appendStar(repo: SearchRepoResponse['items'][0], token: string) {
+  const userStarMap = getRepoStarMap(token);
+  return {
+    ...repo,
+    star: userStarMap.has(repo.full_name),
+  };
+}
+
+// in practice, the `token` here should be pass in by server layer
+export async function searchRepo(token: string, query: string, page = 1) {
   let url = "https://api.github.com/search/repositories?sort=stars&order=desc";
 
   if (query && query.length) {
@@ -45,12 +55,39 @@ export async function searchRepo(query: string, page = 1) {
   try {
     const result: SearchRepoResponse = await r.json();
     return {
-      items: result.items,
+      items: result.items.map(it => appendStar(it, token)),
       resumeTime,
       total: result.total_count,
       page
     };
   } catch (error) {
     throw { resumeTime, error, isRateLimit: false };
+  }
+}
+
+const tokenMap = new Map<string, Map<string, boolean>>();
+
+function getRepoStarMap(token: string) {
+  let map = tokenMap.get(token);
+  if (!map) {
+    map = new Map<string, boolean>();
+    tokenMap.set(token, map);
+  }
+  return map;
+}
+
+
+export async function starRepo(token: string, repoFullName: string, star: boolean) {
+
+  const userStarMap = getRepoStarMap(token);
+  if (star) {
+    userStarMap.set(repoFullName, true);
+  } else {
+    userStarMap.delete(repoFullName);
+  }
+  return {
+    status: 'success',
+    repoFullName,
+    star,
   }
 }
